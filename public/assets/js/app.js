66601ba34b747850a9c0630aed39734c36012635
Santino <ha.agharbi@gmail.com>
2019-03-17 00:23:19 -0700

    fixed js logic

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,42 +1,17 @@
-var app = {
-    devourBurger: function (id) {
-        $.ajax({
-            url: `/api/burger/${id}`,
-            type: "PUT",
-            success: function () {
-                window.location.reload();
-            }
-        })
-    },
-
-    bindUIActions: function () {
-        $(".devour-btn").on("click", function () {
-            var id = $(this).attr("data-burger");
-            app.devourBurger(id);
-        });
-
-        $("#addBurger").on("click", function (e) {
-            e.preventDefault();
-            var data = {
-                name: $("#burgerName").val()
-            };
-            $.ajax({
-                type: "POST",
-                url: "api/burger",
-                data: data,
-                success: function (result) {
-                    console.log(result);
-                    window.location.reload();
-                }
-            });
-        });
-    },
-
-    init: function () {
-        this.bindUIActions();
-    }
-};
-
 $(document).ready(function () {
-    app.init();
+
+    $(".devour-form").on("submit", function (event) {
+        event.preventDefault();
+
+        var burger_id = $(this).children(".burger_id").val();
+        console.log(burger_id)
+        $.ajax({
+            method: "PUT",
+            url: "/burgers/" + burger_id
+        }).then(function (data) {
+            // reload page to display devoured burger in proper column
+            location.reload();
+        })
+
+    })
 });
