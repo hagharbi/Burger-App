b608e6aeeca79b4022b13037df55c28dca44f736
Santino <ha.agharbi@gmail.com>
2019-04-09 15:09:12 -0700

    fixed the update bug

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
-    extended: false
+// Parse application body
+app.use(express.urlencoded({
+    extended: true
 }));
-// parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
+
 
 var exphbs = require("express-handlebars");
 
@@ -26,5 +26,5 @@ var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function () {
-    console.log("Listening on port:%s", PORT);
+    console.log("Listening on port: ", PORT);
 });
